9d39f93985722ce3ac61245b1eb70ba56b61cf32
ALPELA <aplam@buffalo.edu>
2020-10-15 17:00:27 -0400

    Update app.module.ts
    Removed old components that weren't neccessary for app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
-import { ColorWheelComponent } from './color-tester/color-wheel/color-wheel.component';
-import { CombinationComponent } from './color-tester/combination/combination.component';
 import { AboutComponent } from './about/about.component';
 import { ColorTesterComponent } from './color-tester/color-tester.component';
 import { ColorAboutComponent } from './about/color-about/color-about.component';
@@ -13,7 +11,6 @@ import { HarmonyComponent } from './about/harmony/harmony.component';
 import { SymbolismComponent } from './about/symbolism/symbolism.component';
 import { ColorComponent } from './color/color.component';
 import { ColorSymbolismComponent } from './color-symbolism/color-symbolism.component';
-import iro from '@jaames/iro';
 import { GeneralComponent } from './color-symbolism/general/general.component';
 import { EasternComponent } from './color-symbolism/eastern/eastern.component';
 import { WesternComponent } from './color-symbolism/western/western.component'
@@ -30,8 +27,6 @@ import { TriadComponent } from './color-tester/combination/triad/triad.component
   declarations: [
     AppComponent,
     HeaderComponent,
-    ColorWheelComponent,
-    CombinationComponent,
     AboutComponent,
     ColorTesterComponent,
     ColorAboutComponent,
