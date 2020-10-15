bdfcc3fdb053ec1e403440cd1ca76340ecc81a19
ALPELA <aplam@buffalo.edu>
2020-10-15 19:23:55 -0400

    Algorithm for color relationship searching finished
    Finished making function for checking if an combination of buttons yields a certain color relationship, minor styling changes as well.

--- src/app/color-tester/color-tester.component.ts
+++ src/app/color-tester/color-tester.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-color-tester',
@@ -7,12 +8,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorTesterComponent implements OnInit {
   colors = [];
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private router: Router) { 
     for(var i = 0; i <= 11; i++){
       this.colors.push(false);
     }
+  }
+
+  ngOnInit(): void {
+    this.router.navigate(['/color-tester']);
+  }
+
+  checkRelationship(){
+    var firstTrue = -1;
+    var checker =  [];
+
+    for(var i = 0; i <= 11; i++){
+      if(this.colors[i]){
+        firstTrue = i;
+        for(var j = i+1; j <= 11; j++){
+          if(this.colors[j]){
+            checker.push(j);
+          }
+        }
+        break;
+      }
+    }
+
+
+
+    if(checker[0] === firstTrue+1 && checker[1] === firstTrue+11 && checker.length === 2
+    || checker[0] === firstTrue+1 && checker[1] === firstTrue+2 && checker.length === 2
+    || checker[0] === firstTrue+10 && checker[1] === firstTrue+11 && checker.length === 2){  
+      this.router.navigate(['/color-tester/analogous']);
+    }
+
+    else if(checker[0] === firstTrue+6 && checker.length === 1){
+      this.router.navigate(['/color-tester/complementary']);
+    }
+
+    else if(checker[0] === firstTrue+4 && checker[1] === firstTrue+6 &&  checker[2] === firstTrue+10 && checker.length === 3 
+      || checker[0] === firstTrue+2 && checker[1] === firstTrue+6 &&  checker[2] === firstTrue+8 && checker.length === 3 ){
+      
+      this.router.navigate(['/color-tester/doublecomplementary']);
+    }
+
+    else if(checker[0] === firstTrue+2 && checker[1] === firstTrue+7 && checker.length === 2
+      || checker[0] === firstTrue+5 && checker[1] === firstTrue+7 && checker.length === 2){
+      
+      this.router.navigate(['/color-tester/splitcomplementary']);
+    }
+
+    else if(checker[0] === firstTrue+4 && checker[1] === firstTrue+8 && checker.length === 2){
+      
+      this.router.navigate(['/color-tester/triad']);
+    }
+
+    else if (firstTrue !== -1 && checker.length === 0){
+      this.router.navigate(['/color-tester/monochromatic']);
+    }
+
+    else{
+      this.router.navigate(['/color-tester']);
+      console.log(checker);
+      console.log(firstTrue);
+    }
+  }
+
+  toggleTrue(id: number){
+    this.colors[id] = !this.colors[id];
+    var button = document.getElementById(id.toString());
+    if(this.colors[id] === true){
+      button.style.border = "10px";
+      button.style.borderColor = "white";
+      button.style.borderStyle = "solid";
+    }
+    else{
+      button.style.borderStyle = "none";
+    }
+    this.checkRelationship();
   }
 
 }
